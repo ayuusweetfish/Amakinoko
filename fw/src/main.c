bc88169f8a51efde83c171769cbfb0a7a0c989d0
Ayu <claire@ayu.land>
2024-11-02 00:20:38 +0800

    mumu: Running Fibonacci on MCU

--- fw/src/main.c
+++ fw/src/main.c
@@ -355,28 +355,29 @@ if (1) {
   static mumu_vm_t m;
 
   static const uint32_t c[] = {
-    0x22000001, // subn 0, 0, 1
+    0x30010000, // movi 1, 0
+    0x30020001, // movi 2, 1
+    0x30040000, // movi 4, 0
+                // A:
+    0x10030101, // mov 3, 1
+    0x10010202, // mov 1, 2
+    0x11020103, // add 2, 1, 3
+    0x32000001, // subi 0, 1
+    0x910004fb, // brine 0, 4, Ab
+    0x10000101, // mov 0, 1
     0x00000000, // sc 0
   };
-  m.c = c;
-  m.pc = 0;
 
-  m.m[0] = 0xaa558800;
   swv_printf("running!\n");
-  mumu_run(&m);
-  swv_printf("%08x\n", m.m[0]); // 0xaa5587ff
-
-  static uint32_t c2[100] = { 0 };
-  for (int i = 0; i < 99; i++) c2[i] = 0x22000001;
-  m.c = c2;
-  m.m[0] = 0x00000000;
+  m.c = c;
   uint32_t t0 = HAL_GetTick();
   for (int i = 0; i < 10000; i++) {
     m.pc = 0;
-    mumu_run(&m);
+    m.m[0] = 15;
+    mumu_run(&m); // 80 instructions
   }
-  swv_printf("%u\n", HAL_GetTick() - t0); // 961 = 1.04M instructions per second
-  swv_printf("%08x\n", m.m[0]); // -99 * 10000 = 0xfff0e4d0
+  swv_printf("%u\n", HAL_GetTick() - t0); // 758 = 1.05M instructions per second
+  swv_printf("%08x\n", m.m[0]); // 610 = 0x00000262
 }
 
 if (0) {
