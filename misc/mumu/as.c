dd93face99e780470b776d7528e536a3b90d3015
Ayu <claire@ayu.land>
2024-11-02 21:53:56 +0800

    mumu: 🚧 Parse mnemonics

--- misc/mumu/as.c
+++ misc/mumu/as.c
@@ -51,13 +51,60 @@ operand_t read_operand()
   return o;
 }
 
+enum mnemonic_word {
+  MN_WORD_BASE = 0x80,
+  MN_MOV, MN_ADD, MN_SUB, MN_MUL,
+  MN_AND, MN_OR, MN_XOR, MN_BIC, MN_LSR, MN_ASR, MN_LSL,
+  MN_LD, MN_ST, MN_LDC, MN_LDCR,
+  MN_BR, MN_BA,
+  MN_SC,
+};
+static uint8_t mnemonic_trie[36][27];
+
+static void init_trie()
+{
+  static struct {
+    const char *w;
+    enum mnemonic_word id;
+  } mnemonics[] = {
+    {"mov", MN_MOV},
+    {"add", MN_ADD},
+    {"sub", MN_SUB},
+    {"mul", MN_MUL},
+    {"and", MN_AND},
+    {"or", MN_OR},
+    {"xor", MN_XOR},
+    {"bic", MN_BIC},
+    {"lsr", MN_LSR},
+    {"asr", MN_ASR},
+    {"lsl", MN_LSL},
+    {"ld", MN_LD},
+    {"st", MN_ST},
+    {"ldc", MN_LDC},
+    {"ldcr", MN_LDCR},
+    {"br", MN_BR},
+    {"ba", MN_BA},
+    {"sc", MN_SC},
+  };
+
+  uint8_t trie_size = 1;
+  for (uint8_t i = 0; i < sizeof mnemonics / sizeof mnemonics[0]; i++) {
+    uint8_t p = 0;
+    for (const char *w = mnemonics[i].w; *w != 0; w++) {
+      uint8_t q = mnemonic_trie[p][*w - 'a' + 1];
+      if (q == 0)
+        q = mnemonic_trie[p][*w - 'a' + 1] = trie_size++;
+      p = q;
+      printf("%c %d\n", *w, p);
+    }
+    mnemonic_trie[p][0] = mnemonics[i].id;
+  }
+}
+
 uint32_t assemble(uint32_t *restrict rom, uint32_t limit)
 {
   uint32_t len = 0;
   int c;
-
-  static const uint8_t mnemonic_trie[36][26] = {
-  };
 
   while (1) {
     while ((c = getchar()) != EOF && isspace(c)) { }
@@ -67,7 +114,12 @@ uint32_t assemble(uint32_t *restrict rom, uint32_t limit)
     uint8_t trie_pos = 0;
     putchar('[');
     do {
+      c = tolower(c);
       putchar(c);
+      trie_pos = mnemonic_trie[trie_pos][c - 'a' + 1];
+      if (trie_pos == 0) {
+        printf("Invalid mnemonic\n");
+      }
     } while ((c = getchar()) != EOF && isalpha(c));
     putchar(']'); putchar('\n');
 
@@ -84,6 +136,8 @@ uint32_t assemble(uint32_t *restrict rom, uint32_t limit)
 
 int main()
 {
+  init_trie();
+
   uint32_t c[256];
   uint32_t len = assemble(c, sizeof c / sizeof c[0]);
 
