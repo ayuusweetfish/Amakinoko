97b75b649ed1b4b5e765cb1a49d1f1e7aae1211b
Ayu <claire@ayu.land>
2024-11-04 16:31:26 +0800

    mumu-as: Include column index in error report

--- misc/mumu/as.c
+++ misc/mumu/as.c
@@ -2,23 +2,6 @@
 #include <stdint.h>
 #include <stdio.h>
 
-typedef struct {
-  enum operand_type_t {
-    REGISTER,
-    IMMEDIATE,
-    LABEL_B,
-    LABEL_F,
-    NONE,
-  } ty;
-  uint32_t n;
-} operand_t;
-
-typedef struct {
-  uint8_t opcode;
-  uint8_t n_operands;
-  operand_t a, b, c;
-} instruction_t;
-
 enum mnemonic_word {
   MN_B_AL = 0x00, MN_B_EQ, MN_B_NE, MN_B_GT, MN_B_LT, MN_B_GE, MN_B_LE,
   MN_B_SGT, MN_B_SLT, MN_B_SGE, MN_B_SLE,
@@ -100,38 +83,48 @@ static void init_trie()
   }
 }
 
-static uint32_t line_num;
+static uint32_t line_num, col_num, saved_col_num;
 
 static inline int my_getchar()
 {
   int c = getchar();
-  if (c == '\n') line_num++;
+  if (c == '\n') { line_num++; saved_col_num = col_num; col_num = 0; }
+  else col_num++;
   return c;
 }
 static inline void my_ungetchar(int c)
 {
   ungetc(c, stdin);
-  if (c == '\n') line_num--;
+  if (c == '\n') { line_num--; col_num = saved_col_num; }
+  else col_num--;
 }
+
+struct file_pos_t {
+  uint32_t line, col;
+};
 
 uint32_t assemble(
   uint32_t *restrict rom, uint32_t limit,
-  uint32_t *o_err_line, char *o_err_msg, uint32_t err_msg_len_limit)
+  struct file_pos_t *o_err_pos, char *o_err_msg, uint32_t err_msg_len_limit)
 {
   uint32_t n_assembled = 0;
   int c;
   line_num = 1;
+  col_num = 0;
 
   uint32_t backward_labels[26];
   for (uint8_t i = 0; i < 26; i++) backward_labels[i] = (uint32_t)-1;
-  uint32_t forward_patch[26][32], forward_patch_first_ln[26];
+  uint32_t forward_patch[26][32];
+  struct file_pos_t forward_patch_first_pos[26];
   uint8_t forward_patch_n[26] = { 0 };
 
-#define report_error(_line, _fmt, ...) do { \
+#define report_error_pos(_pos, _fmt, ...) do { \
   snprintf(o_err_msg, err_msg_len_limit, _fmt, ##__VA_ARGS__); \
-  *o_err_line = (_line); \
+  *o_err_pos = (_pos); \
   return n_assembled; \
 } while (0)
+#define report_error(_line, _fmt, ...) \
+  report_error_pos(((struct file_pos_t){ .line = (_line), .col = 0 }), _fmt, ##__VA_ARGS__)
 
 #define emit_24(_opcode, _v) do { \
   if (n_assembled >= limit) report_error(line_num, "Too many instructions"); \
@@ -178,7 +171,10 @@ uint32_t assemble(
       for (uint8_t i = 0; i < forward_patch_n[label_id]; i++) {
         uint32_t addr = forward_patch[label_id][i];
         if (n_assembled - (addr + 1) > 0xff)
-          report_error(instr_ln, "Label %c too far away", 'A' + label_id);
+          report_error(instr_ln, "Label %c too far away from forward-reference (%u:%u)",
+            'A' + label_id,
+            forward_patch_first_pos[label_id].line,
+            forward_patch_first_pos[label_id].col);
         rom[addr] |= (n_assembled - (addr + 1));
       }
       forward_patch_n[label_id] = 0;
@@ -186,16 +182,29 @@ uint32_t assemble(
     }
 
     // Operands
+    typedef struct {
+      enum operand_type_t {
+        REGISTER,
+        IMMEDIATE,
+        LABEL_B,
+        LABEL_F,
+        NONE,
+      } ty;
+      uint32_t n;
+    } operand_t;
+
     uint8_t n_operands = 0;
     operand_t operands[3];
+    struct file_pos_t operands_pos[3];
     while (1) {
       operand_t o;
       int c;
-      uint32_t op_ln = line_num;
 
       // Find operand
       while ((c = my_getchar()) != EOF && c != '\n' && c != '%' && c != '=' && !isalpha(c)) { }
       if (c == EOF || c == '\n') break;
+
+      struct file_pos_t start_pos = { .line = line_num, .col = col_num };
       if (c == '%' || c == '=') {
         o.ty = (c == '%' ? REGISTER : IMMEDIATE);
         uint32_t n = 0;
@@ -203,7 +212,7 @@ uint32_t assemble(
           n = n * 10 + (c - '0');
           uint32_t limit = (o.ty == REGISTER ? ((uint32_t)1 << 8) : ((uint32_t)1 << 24));
           if (n >= limit)
-            report_error(op_ln, "%s out of range: expected <= %u",
+            report_error_pos(start_pos, "%s out of range: expected <= %u",
               o.ty == REGISTER ? "Register index" : "Immediate value",
               limit - 1);
         }
@@ -216,24 +225,27 @@ uint32_t assemble(
         } else if (dir == 'f') {
           o.ty = LABEL_F;
         } else {
-          report_error(op_ln, "Invalid direction %c\n", dir);
+          report_error_pos(start_pos, "Invalid direction %c\n", dir);
         }
         o.n = tolower(c) - 'a';
       }
 
       if (n_operands >= 3) {
-        report_error(op_ln, "Extraneous operand");
+        report_error_pos(start_pos, "Extraneous operand");
       } else {
+        operands_pos[n_operands] = start_pos;
         operands[n_operands++] = o;
       }
     }
 
     // Emit code
     if (mnemonic_valid && mnemonic > 0) {
-      #define ensure_imm_range(_imm, _bits) do { \
-        if ((_imm) >= ((uint32_t)1 << (_bits))) \
-          report_error(instr_ln, "Immediate value %u out of range: expected <= %u", \
-            (_imm), ((uint32_t)1 << (_bits)) - 1); \
+      #define ensure_imm_range(_op_id, _bits) do { \
+        uint32_t imm = operands[(_op_id)].n; \
+        if (imm >= ((uint32_t)1 << (_bits))) \
+          report_error_pos(operands_pos[(_op_id)], \
+            "Immediate value %u out of range: expected <= %u", \
+            imm, ((uint32_t)1 << (_bits)) - 1); \
       } while (0)
       if (mnemonic >= MN_MOV && mnemonic <= MN_LSL) {
         if (n_operands == 3 &&
@@ -245,7 +257,7 @@ uint32_t assemble(
             operands[0].ty == REGISTER &&
             operands[1].ty == REGISTER &&
             operands[2].ty == IMMEDIATE) {
-          ensure_imm_range(operands[2].n, 8);
+          ensure_imm_range(2, 8);
           emit(0x20 + (mnemonic - MN_MOV), operands[0].n, operands[1].n, operands[2].n);
         } else if (n_operands == 2 &&
             operands[0].ty == REGISTER &&
@@ -254,7 +266,7 @@ uint32_t assemble(
         } else if (n_operands == 2 &&
             operands[0].ty == REGISTER &&
             operands[1].ty == IMMEDIATE) {
-          ensure_imm_range(operands[1].n, 16);
+          ensure_imm_range(1, 16);
           emit_bh(0x30 + (mnemonic - MN_MOV), operands[0].n, operands[1].n);
         } else {
           report_error(instr_ln, "Invalid operands");
@@ -264,7 +276,7 @@ uint32_t assemble(
             operands[0].ty == REGISTER &&
             operands[1].ty == REGISTER &&
             operands[2].ty == IMMEDIATE) {
-          ensure_imm_range(operands[2].n, 8);
+          ensure_imm_range(2, 8);
           emit(0x70 + (mnemonic - MN_LD), operands[0].n, operands[1].n, operands[2].n);
         } else {
           report_error(instr_ln, "Invalid operands");
@@ -283,16 +295,16 @@ uint32_t assemble(
           if (operands[2].ty == LABEL_B) {
             uint32_t last = backward_labels[operands[2].n];
             if (last == (uint32_t)-1)
-              report_error(instr_ln, "Label %c not found", 'A' + operands[2].n);
+              report_error_pos(operands_pos[2], "Label %c not found", 'A' + operands[2].n);
             if (last + 0x80 < n_assembled + 1)
-              report_error(instr_ln, "Label %c too far away", 'A' + operands[2].n);
+              report_error_pos(operands_pos[2], "Label %c too far away", 'A' + operands[2].n);
             offset = (last - (n_assembled + 1)) & 0xff;
           } else {  // LABEL_F
             if (forward_patch_n[operands[2].n] >= sizeof forward_patch[0])
-              report_error(instr_ln, "Too many unresolved forward-referencing labels %cf", 'A' + operands[2].n);
+              report_error_pos(operands_pos[2], "Too many unresolved forward-referencing labels %cf", 'A' + operands[2].n);
             forward_patch[operands[2].n][forward_patch_n[operands[2].n]++] = n_assembled;
             if (forward_patch_n[operands[2].n] == 1)
-              forward_patch_first_ln[operands[2].n] = instr_ln;
+              forward_patch_first_pos[operands[2].n] = operands_pos[2];
           }
           emit(0x80 + (mnemonic - MN_BR), operands[0].n, operands[1].n, offset);
         } else {
@@ -301,12 +313,13 @@ uint32_t assemble(
       } else if (mnemonic == MN_SC) {
         if (n_operands == 1 &&
             operands[0].ty == IMMEDIATE) {
-          ensure_imm_range(operands[0].n, 24);
+          ensure_imm_range(0, 24);
           emit_24(0x00, operands[0].n);
         } else {
           report_error(instr_ln, "Invalid operands");
         }
       } else {
+        // Should not happen!
         report_error(instr_ln, "Unsupported instruction");
       }
     }
@@ -314,9 +327,9 @@ uint32_t assemble(
 
   for (uint8_t i = 0; i < 26; i++)
     if (forward_patch_n[i] != 0)
-      report_error(forward_patch_first_ln[i], "Label not found");
+      report_error_pos(forward_patch_first_pos[i], "Label not found");
 
-  *o_err_line = 0;
+  *o_err_pos = (struct file_pos_t){ 0 };
   return n_assembled;
 }
 
@@ -324,16 +337,16 @@ int main()
 {
   init_trie();
 
-  uint32_t c[256];
-  uint32_t err_line;
+  uint32_t c[1024];
+  struct file_pos_t err_pos;
   char err_msg[64];
-  uint32_t len = assemble(c, sizeof c / sizeof c[0], &err_line, err_msg, sizeof err_msg);
+  uint32_t len = assemble(c, sizeof c / sizeof c[0], &err_pos, err_msg, sizeof err_msg);
 
   for (uint32_t i = 0; i < len; i++)
     printf("%04x: %08x\n", i, c[i]);
 
-  if (err_line != 0)
-    printf("line %u: %s\n", err_line, err_msg);
+  if (err_pos.line != 0)
+    printf("(%u:%u): %s\n", err_pos.line, err_pos.col, err_msg);
 
   return 0;
 }
