87e6c69bfec2af7d6c2b082d73b03199e7b3e490
Ayu <claire@ayu.land>
2024-11-05 16:28:20 +0800

    cfg: Build with libserialport

--- cfg/main.c
+++ cfg/main.c
@@ -1,5 +1,7 @@
 #include "libui/ui.h"
+#include "libserialport/libserialport.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 static uiWindow *w;
 
@@ -11,6 +13,20 @@ static int windowOnClosing(uiWindow *w, void *_unused)
 {
   uiQuit();
   return 1;
+}
+
+static void list_serial_ports()
+{
+  struct sp_port **port_list;
+  if (sp_list_ports(&port_list) < 0) {
+    fprintf(stderr, "Cannot retrieve list of ports\n");
+    exit(0);
+  }
+  int i;
+  for (i = 0; port_list[i] != NULL; i++)
+    fprintf(stderr, "%d: %s\n", i, sp_get_port_name(port_list[i]));
+  fprintf(stderr, "%d serial port(s) total\n", i);
+  sp_free_port_list(port_list);
 }
 
 int main()
@@ -31,6 +47,8 @@ int main()
   // Create window
   w = uiNewWindow("蘑菇！", 480, 600, 0);
   uiWindowSetMargined(w, 1);
+
+  list_serial_ports();
 
   // Run main loop
   uiWindowOnClosing(w, windowOnClosing, NULL);
