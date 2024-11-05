5c7001cf6e92e14723cf7f26ebe92d999992d6e3
Ayu <claire@ayu.land>
2024-11-05 21:04:45 +0800

    cfg: A first combo box...?

--- cfg/main.c
+++ cfg/main.c
@@ -29,6 +29,20 @@ static void list_serial_ports()
   sp_free_port_list(port_list);
 }
 
+static void btn_serial_port_refresh_clicked(uiButton *btn, void *_cbox_serial_port)
+{
+  uiCombobox *cbox_serial_port = (uiCombobox *)_cbox_serial_port;
+  struct sp_port **port_list;
+  if (sp_list_ports(&port_list) < 0) {
+    fprintf(stderr, "Cannot retrieve list of ports\n");
+    exit(0);
+  }
+  for (int i = 0; port_list[i] != NULL; i++) {
+    uiComboboxAppend(cbox_serial_port, sp_get_port_name(port_list[i]));
+  }
+  sp_free_port_list(port_list);
+}
+
 int main()
 {
   uiInitOptions o = { 0 };
@@ -40,13 +54,40 @@ int main()
   }
 
 #if __APPLE__
-  uiMenu *menuDefault = uiNewMenu("");
-  uiMenuAppendQuitItem(menuDefault);
+  {
+    uiMenu *menu_default = uiNewMenu("");
+    uiMenuAppendQuitItem(menu_default);
+  }
 #endif
 
   // Create window
   w = uiNewWindow("蘑菇！", 480, 600, 0);
   uiWindowSetMargined(w, 1);
+
+  // Layout
+  uiBox *box_main = uiNewVerticalBox();
+  uiBoxSetPadded(box_main, 1);
+  uiWindowSetChild(w, uiControl(box_main));
+
+  uiBox *box_serial_port = uiNewVerticalBox();
+  uiBoxSetPadded(box_serial_port, 1);
+  uiBoxAppend(box_main, uiControl(box_serial_port), 0);
+  {
+    uiBox *box_serial_port_r1 = uiNewHorizontalBox();
+    uiBoxSetPadded(box_serial_port_r1, 1);
+    uiBoxAppend(box_serial_port, uiControl(box_serial_port_r1), 0);
+    {
+      uiLabel *lbl = uiNewLabel("串口");
+      uiBoxAppend(box_serial_port_r1, uiControl(lbl), 0);
+
+      uiCombobox *cbox_serial_port = uiNewCombobox();
+      uiBoxAppend(box_serial_port_r1, uiControl(cbox_serial_port), 1);
+
+      uiButton *btn_serial_port_refresh = uiNewButton("刷新");
+      uiBoxAppend(box_serial_port_r1, uiControl(btn_serial_port_refresh), 0);
+      uiButtonOnClicked(btn_serial_port_refresh, btn_serial_port_refresh_clicked, cbox_serial_port);
+    }
+  }
 
   list_serial_ports();
 
