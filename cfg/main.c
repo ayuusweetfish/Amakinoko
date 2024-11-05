fef56b0d54a4db25874241eba60413be2324232e
Ayu <claire@ayu.land>
2024-11-05 21:50:23 +0800

    cfg: Working combo box with a patch

--- cfg/main.c
+++ cfg/main.c
@@ -2,6 +2,7 @@
 #include "libserialport/libserialport.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 static uiWindow *w;
 
@@ -29,29 +30,46 @@ static void list_serial_ports()
   sp_free_port_list(port_list);
 }
 
-static uiGrid *box_serial_port_r1;
-static uiCombobox *cbox_serial_port = NULL;
-static void btn_serial_port_refresh_clicked(uiButton *btn, void *_unused)
+static int port_names_n = 0;
+static char **port_names = NULL;
+static void btn_serial_port_refresh_clicked(uiButton *btn, void *_cbox_serial_port)
 {
-  if (cbox_serial_port != NULL) {
-    // XXX: This does not work due to `uiGrid`-maintained children list going out-of-sync
-    uiControlSetParent(uiControl(cbox_serial_port), NULL);
-    uiControlDestroy(uiControl(cbox_serial_port));
-  }
+  uiCombobox *cbox_serial_port = (uiCombobox *)_cbox_serial_port;
 
-  cbox_serial_port = uiNewCombobox();
-  uiGridAppend(box_serial_port_r1, uiControl(cbox_serial_port),
-    1, 0, 1, 1, 1, uiAlignFill, 0, uiAlignFill);
+  int last_sel = uiComboboxSelected(cbox_serial_port);
+  char *last_sel_name = NULL;
+  if (last_sel >= 0 && last_sel < port_names_n)
+    last_sel_name = port_names[last_sel];
+  uiComboboxClear(cbox_serial_port);
 
   struct sp_port **port_list;
   if (sp_list_ports(&port_list) < 0) {
     fprintf(stderr, "Cannot retrieve list of ports\n");
     exit(0);
   }
+  int n;
+  for (n = 0; port_list[n] != NULL; n++) { }
+  char **new_names = malloc(sizeof(char *) * n);
+
+  int new_sel = -1;
   for (int i = 0; port_list[i] != NULL; i++) {
-    uiComboboxAppend(cbox_serial_port, sp_get_port_name(port_list[i]));
+    new_names[i] = strdup(sp_get_port_name(port_list[i]));
+    uiComboboxAppend(cbox_serial_port, new_names[i]);
+    if (last_sel_name != NULL && strcmp(last_sel_name, new_names[i]) == 0)
+      new_sel = i;
   }
+
   sp_free_port_list(port_list);
+
+  if (new_sel != -1)
+    uiComboboxSetSelected(cbox_serial_port, new_sel);
+
+  if (port_names != NULL) {
+    for (int i = 0; i < port_names_n; i++) free(port_names[i]);
+    free(port_names);
+  }
+  port_names_n = n;
+  port_names = new_names;
 }
 
 int main()
@@ -84,19 +102,20 @@ int main()
   uiBoxSetPadded(box_serial_port, 1);
   uiBoxAppend(box_main, uiControl(box_serial_port), 0);
   {
-    box_serial_port_r1 = uiNewGrid();
-    uiGridSetPadded(box_serial_port_r1, 1);
+    uiBox *box_serial_port_r1 = uiNewHorizontalBox();
+    uiBoxSetPadded(box_serial_port_r1, 1);
     uiBoxAppend(box_serial_port, uiControl(box_serial_port_r1), 0);
     {
       uiLabel *lbl = uiNewLabel("串口");
-      uiGridAppend(box_serial_port_r1, uiControl(lbl),
-        0, 0, 1, 1, 0, uiAlignFill, 0, uiAlignFill);
+      uiBoxAppend(box_serial_port_r1, uiControl(lbl), 0);
+
+      uiCombobox *cbox_serial_port = uiNewCombobox();
+      uiBoxAppend(box_serial_port_r1, uiControl(cbox_serial_port), 1);
 
       uiButton *btn_serial_port_refresh = uiNewButton("刷新");
-      uiGridAppend(box_serial_port_r1, uiControl(btn_serial_port_refresh),
-        2, 0, 1, 1, 0, uiAlignFill, 0, uiAlignFill);
-      uiButtonOnClicked(btn_serial_port_refresh, btn_serial_port_refresh_clicked, NULL);
-      btn_serial_port_refresh_clicked(btn_serial_port_refresh, NULL);
+      uiBoxAppend(box_serial_port_r1, uiControl(btn_serial_port_refresh), 0);
+      uiButtonOnClicked(btn_serial_port_refresh, btn_serial_port_refresh_clicked, cbox_serial_port);
+      btn_serial_port_refresh_clicked(btn_serial_port_refresh, cbox_serial_port);
     }
   }
 
